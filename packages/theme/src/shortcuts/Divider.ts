7d3159a06a360f7eda79db513ec0b4c090801fe1
AntzyMo <mozbano@163.com>
2024-08-26 10:29:23 +0800

    chore:  lt-divider-text px-2

--- packages/theme/src/shortcuts/Divider.ts
+++ packages/theme/src/shortcuts/Divider.ts
@@ -1,6 +1,6 @@
 export const Divider = {
   'lt-divider': 'relative m-0 p-0 clear-both border-gray-200',
-  'lt-divider-text': 'px-5 absolute -translate-y-52% bg-white',
+  'lt-divider-text': 'px-2 absolute -translate-y-52% bg-white',
   // divider type
   'lt-divider--horizontal': 'my-4 h-1px w-full border-t lt-divider--horizontal-solid',
   'lt-divider--vertical': 'mx-2 w-1px h-.8em -mt-2px border-l lt-divider--vertical-solid inline-block align-middle',
