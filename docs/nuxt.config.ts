36cf2909bbf01b5ecb2bd7baaff3df8a56a5e80d
AntzyMo <mozbano@163.com>
2024-09-04 15:04:52 +0800

    feat: nuxthub

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@nuxt/content', '@vueuse/nuxt', '@nuxt/image'],
+  modules: ['@unocss/nuxt', '@nuxt/content', '@vueuse/nuxt', '@nuxt/image', '@nuxthub/core'],
   css: ['@unocss/reset/tailwind.css'],
   content: {
     highlight: {
