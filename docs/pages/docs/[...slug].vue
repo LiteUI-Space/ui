b2e31b666aa22000b38a0bb8ceb5ed16383b846b
AntzyMo <mozbano@163.com>
2024-09-09 11:12:49 +0800

    update: docs

--- docs/pages/docs/[...slug].vue
+++ docs/pages/docs/[...slug].vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
-  import { Breadcrumb } from '@lite-space/ui'
+  import { Breadcrumb, Menu, MenuItem } from '@lite-space/ui'
 
   const route = useRoute()
   const { data: page, error } = await useAsyncData(route.path, () => queryContent(route.path).findOne())
   const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
+  const url = useRequestURL()
 </script>
 
 <template>
   <div flex="~" class="gap-10">
     <template v-if="page?.title">
       <aside class="sticky top-14 z-1">
-        <div v-for="item in navigation" :key="item._path">
-          <div v-for="it in item.children" :key="it._path">
-            <h3 class="text-sm font-bold mb-3">
-              {{ it.title }}
-            </h3>
-            <div class="grid gap-1">
-              <div v-for="t in it.children" :key="t._path" class="text-sm text-gray-600">
-                <NuxtLink :to="t._path" class="hover:text-gray-800">
+        <template v-for="item in navigation" :key="item._path">
+          <Menu :model-value="url.pathname" open>
+            <MenuItem v-for="it in item.children" :key="it._path" :value="it._path">
+              <template #title>
+                <h3 class="text-sm font-bold">
+                  {{ it.title }}
+                </h3>
+              </template>
+              <MenuItem
+                v-for="t in it.children"
+                :key="t._path"
+                :value="t._path"
+              >
+                <NuxtLink
+                  :to="t._path"
+                  class="w-full hover:text-gray-800 text-sm text-gray-600"
+                >
                   {{ t.title }}
                 </NuxtLink>
-              </div>
-            </div>
-          </div>
-        </div>
+              </MenuItem>
+            </MenuItem>
+          </Menu>
+        </template>
       </aside>
       <main class="relative">
         <Breadcrumb class="" :items="['Docs', page.title]" separator="i-carbon:chevron-right" />
